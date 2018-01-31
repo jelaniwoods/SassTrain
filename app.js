68ddbc6b506930cb0d25c57633a5db4fc7446fea
baby snaps <jmw686@nyu.edu>
2018-01-31 16:59:25 -0600

    change jade to pug, set up sessions, begin to add passport auth

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+//TODO cookieParser might be conflicting with new express-session
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const session = require('express-session');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+const sess = {
+  secret: 'S3CR37',
+  cookie: {
+    maxAge: 60000
+  }
+}
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  // we're connected!
+  console.log('connection ongoing~');
+});
+
+app.use(session(sess));
+app.use(passport.initialize());
+app.use(passport.session());
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  if (req.session.views) {
+    req.session.views++;
+  } else {
+    req.session.views = 1;
+  }
+  console.log(req.session.views);
+  res.render('index', { title: 'SassTrain', user: req.user, views: req.session.views });
+});
+
+module.exports = router;
