4a6aa457d2c21b22502c4c13e63e1ebc857cb7e9
baby snaps <jmw686@nyu.edu>
2018-02-05 13:57:07 -0600

    update & fix forum model

--- models/forum.js
+++ models/forum.js
@@ -1,14 +1,39 @@
 const mongoose = require('mongoose'),
   Schema = mongoose.Schema,
-  Post = require('./post.js'),
-  User = require('./user.js');
   URLSlugs = require('mongoose-url-slugs');
+let Post = require('./post.js'),
+  User = require('./user.js');
 
-//TODO add tags
+//TODO I think this needs to also be in this file, but I'm not sure
+Post = new Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+  username: {type:String, required: false},
+  timestamp: {type:Date,"default":Date.now},
+  content:  {type: String, required: true},
+  image_url:  {type: String, required: false}
+});
+
+
+//TODO add tags, make Category something else...
+// its related to the forum but there should only be ONE of each category
+// searching by category will bring all the Forums of that category up
+// should it be it's own model?
   let Forum = new mongoose.Schema({
     title: {type: String, required: true},
     category: {type: String, required: true},
     description: {type: String, required: false},
-    posts: {type: [Post]}
+    posts: [Post]
 
   });
+
+  Forum.plugin(URLSlugs('title'));
+
+  Forum = module.exports = mongoose.model('forum', Forum);
+
+  module.exports.create = function (newForum, callback) {
+    newForum.save(callback);
+  }
+
+  module.exports.getForumById = function(id, callback){
+	   Forum.findById(id, callback);
+  }
