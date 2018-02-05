fa16010cc2f45b20fbf06a7da15b15f485c12274
baby snaps <jmw686@nyu.edu>
2018-02-04 18:53:46 -0600

    add models for post and forum

---
+++ models/forum.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  Post = require('./post.js'),
+  User = require('./user.js');
+  URLSlugs = require('mongoose-url-slugs');
---
+++ models/post.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  User = require('./user.js'),
+  URLSlugs = require('mongoose-url-slugs');
