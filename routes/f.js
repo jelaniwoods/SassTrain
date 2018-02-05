478f5b51ea84888bc688f1a6cb29a8fa0260a904
baby snaps <jmw686@nyu.edu>
2018-02-05 13:57:40 -0600

    creating forum works, but not finding

--- routes/f.js
+++ routes/f.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-// const User = require('../models/user');
+const Forum = require('../models/forum');
 
-// route will be sasstrain.com/f/
+// route will be host:port/f/
 router.get('/', function(req, res){
-	res.render('forum_home');
+	let result = Forum.find();
+	res.render('forum_home', {result: result});
 });
 
+router.get('/new', function(req, res){
+	res.render('forum_new');
+});
+
+router.post('/new', (req, res) => {
+	let title = req.body.title;
+	let category = req.body.category;
+	let description = req.body.description;
+
+  // Validation
+	req.checkBody('title', 'Title is required').notEmpty();
+	req.checkBody('category', 'Category is required').notEmpty();
+
+	let errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			errors:errors
+		});
+	} else {
+		let newForum = new Forum({
+			title: title,
+			category: category,
+			description: description
+		});
+
+		Forum.create(newForum, (err, forum) => {
+			if(err) throw err;
+			console.log(forum);
+
+		});
+
+		req.flash('success_msg', 'You successfully started a discussion!');
+		res.redirect('/f');
+	}
+});
 router.get('/:slug', function(req, res){
 	res.render('forum_page');
 });
 
 // TODO add search route
+
+module.exports = router;
